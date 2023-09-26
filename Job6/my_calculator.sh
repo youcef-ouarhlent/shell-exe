1632d2c4cf5a4b6fee420786aa4aa970160913c3
root <root@LAPTOP-H9B096GI>
2023-09-26 17:06:50 +0200

    Job6

---
+++ Job6/my_calculator.sh
@@ -0,0 +1,14 @@
+if [ $2 = - ]
+then echo $(( $1 - $3 ))
+
+elif [ $2 = + ]
+then echo $(( $1 + $3 ))
+
+elif [ $2 = / ]
+then  echo $(( $1 / $3 ))
+
+elif [ $2 = * ]
+then echo $(( $1 * $3 ))
+
+fi
+
